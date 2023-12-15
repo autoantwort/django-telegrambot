60b70decc8dd629ebe583183209a07bbb2798958
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2023-12-15 18:20:30 +0100

    Run autopep8

--- django_telegrambot/urls.py
+++ django_telegrambot/urls.py
@@ -3,13 +3,15 @@ from django.urls import re_path
 from . import views
 from django.conf import settings
 
-webhook_base = settings.DJANGO_TELEGRAMBOT.get('WEBHOOK_PREFIX','/')
+webhook_base = settings.DJANGO_TELEGRAMBOT.get('WEBHOOK_PREFIX', '/')
 if webhook_base.startswith("/"):
     webhook_base = webhook_base[1:]
 if not webhook_base.endswith("/"):
     webhook_base += "/"
 
 urlpatterns = [
-    re_path(r'admin/django-telegrambot/$', views.home, name='django-telegrambot'),
-    re_path(r'{}(?P<bot_token>.+?)/$'.format(webhook_base), views.webhook, name='webhook'),
+    re_path(r'admin/django-telegrambot/$',
+            views.home, name='django-telegrambot'),
+    re_path(r'{}(?P<bot_token>.+?)/$'.format(webhook_base),
+            views.webhook, name='webhook'),
 ]
